1fe5fc83c4d30e51c57c9bfad5c6d031bb21cc2c
Lakee Rawat <73211332+lakeerawat@users.noreply.github.com>
2021-12-28 14:56:27 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+
+public class Palindrome
+{
+    public static void Main(string[] args)
+    {
+        (bool, int) IsPalindrome(string thestr)
+        {
+            string strinput;
+            strinput = thestr.ToUpper();
+            var sb = new StringBuilder();
+            foreach (char c in strinput)
+            {
+                if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+                {
+                    sb.Append(c);
+                }
+            }
+            strinput = sb.ToString();
+            int i = 0, j = strinput.Length - 1;
+            while (i <= j)
+            {
+                if (strinput[i] != strinput[j])
+                {
+                    return (false, 0);
+                }
+                i++;
+                j--;
+            }
+            return (true, thestr.Length);
+        }
+
+        string inputstr = "";
+        (bool b, int l) result;
+        Console.WriteLine("Type the input");
+        while (inputstr != "exit")
+        {
+            inputstr = Console.ReadLine();
+            if (inputstr != "exit")
+            {
+                result = IsPalindrome(inputstr);
+                Console.WriteLine($"Status {result.b}, The length of string {result.l}");
+            }
+            break;
+        }
+        Console.ReadLine();
+    }
+}
